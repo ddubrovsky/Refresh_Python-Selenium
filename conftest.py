b344a1e7cc49a8eee0f883c494a3d5d4974e6087
dmitrii <kddima1976@gmail.com>
2024-02-02 13:23:24 -0800

    to fixture added scope='session' for starting browser ones for all session, not for each test separately

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 __author__ = 'Dmitrii'
+
 import pytest
 from fixture.application import Application
 
-@pytest.fixture
+
+@pytest.fixture(scope='session')
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
