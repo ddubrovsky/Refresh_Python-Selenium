76d6104b071b3da5568a2cca6ce97d5e99bdc1bf
dmitrii <kddima1976@gmail.com>
2024-02-01 16:57:59 -0800

    packages have been created for different areas of responsibility

---
+++ test/add_new_group.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*
+
+import pytest
+from fixture.application import Application
+from model.group import Group
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_new_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="new_1", header="bla-bla-bla", footer="bla footer"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
