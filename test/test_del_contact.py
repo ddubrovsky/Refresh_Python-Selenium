935db05ae44a9a936eb35ec130c6e8d7d16e5d3f
dmitrii <kddima1976@gmail.com>
2024-02-13 13:01:01 -0800

    added assert for del group and therefor repr and eq functions in the group model

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -6,7 +6,4 @@ from model.contact import Contact
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    old_groups = app.group.get_group_list()
-    app.contact.delete_first_contact()
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
+        app.contact.delete_first_contact()
