2055657fa9ef5492949af327f63743be4c530b3d
dmitrii <kddima1976@gmail.com>
2024-02-19 18:40:23 -0800

    added assert sorted groups and modified model group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,14 +5,20 @@ from model.group import Group
 
 def test_add_new_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="new_1", header="bla-bla-bla", footer="bla footer"))
+    group = Group(name="new_1", header="bla-bla-bla", footer="bla footer")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     # app.session.logout()
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
