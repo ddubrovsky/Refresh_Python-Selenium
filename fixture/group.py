9c1ae4a90534b0381da1af527b05980ac8a1c30b
dmitrii <kddima1976@gmail.com>
2024-02-27 14:17:15 -0800

    added test delete group by index, delete first group realized throw the index one

--- fixture/group.py
+++ fixture/group.py
@@ -38,10 +38,22 @@ class GroupHelper:
             wd.find_element(By.NAME, field_name).clear()
             wd.find_element(By.NAME, field_name).send_keys(text)
 
+#    def delete_first_group(self):
+#        wd = self.app.wd
+#        self.open_groups_page()
+#        self.select_first_group()
+#        # submit deletion
+#        wd.find_element(By.NAME, "delete").click()
+#        self.return_to_group_page()
+#        self.group_cache = None
+
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element(By.NAME, "delete").click()
         self.return_to_group_page()
@@ -50,6 +62,10 @@ class GroupHelper:
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element(By.NAME, "selected[]").click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements(By.NAME, "selected[]")[index].click()
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
